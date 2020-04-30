3c7a955f9cbbc89a61f637898b632d3f5c0847a7
PumkinQ <nz9024295@gmail.com>
2020-04-29 21:27:36 -0500

    final: mpi

--- MPI/Final/mpi.c
+++ MPI/Final/mpi.c
@@ -9,33 +9,24 @@
 #include <string.h>
 #include <time.h>
 
-long long toss_darts (long long tosses, int rank){
-	long long toss, number_in_circle = 0;        
-	double x,y;
-	unsigned int seed = (unsigned) time(NULL);
-	srand(seed + rank);
-	for (toss = 0; toss < tosses; toss++) {
-	   x = rand_r(&seed)/(double)RAND_MAX;
-	   y = rand_r(&seed)/(double)RAND_MAX;
-	   if( (x*x+y*y) <= 1.0 ){
-           number_in_circle++;
-        }
-    }
-    return number_in_circle;
-}
+// Marcos
+#define MASTER 0      /* rank of the master */
+#define BLOCK_LOW(id,p,n)   ((id)*(n)/(p))
+#define BLOCK_HIGH(id,p,n)  (BLOCK_LOW((id)+1,p,n)-1)
 
 int main(int argc, char *argv[]) {
-   int rank, nprocs;
-   long long number_of_tosses, number_of_toss, number_in_circle, circles;
-   double pi_estimate;
-   double PI25DT = 3.141592653589793238462643;
-   
-   MPI_Init(&argc, &argv);
-   MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
-   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  
-   // Get input from the command line
-   if (argc!= 2){
+    int rank, nprocs;
+    long long i, number_of_tosses, number_in_circle = 0, circles;
+    double pi_estimate;
+    double x, y;
+    double PI25DT = 3.141592653589793238462643;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // Get input from the command line
+    if (argc!= 2){
         printf("usage: mpirun -np <number_of_processes> ./ProgramName <number_of_tosses> \n");
         MPI_Finalize();
         exit(-1);
@@ -43,20 +34,24 @@ int main(int argc, char *argv[]) {
         number_of_tosses = atoi(argv[1]);
     }
 
-   number_of_toss = number_of_tosses/nprocs; 
-   
-   MPI_Barrier(MPI_COMM_WORLD);
+    unsigned int seed = (unsigned) time(NULL);
+	srand(seed + rank);
+    for (i = BLOCK_LOW(rank, nprocs, number_of_tosses); i <= BLOCK_HIGH(rank, nprocs, number_of_tosses); i++) {
+	   x = rand_r(&seed)/(double)RAND_MAX;
+	   y = rand_r(&seed)/(double)RAND_MAX;
+	   if( (x*x+y*y) <= 1.0 ){
+           number_in_circle++;
+        }
+    }
 
-   number_in_circle = toss_darts(number_of_toss, rank);
- 
-   MPI_Reduce(&number_in_circle, &circles, 1, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
-   
-   if (rank == 0) {
-	   pi_estimate = (4*circles)/((double) number_of_tosses);
-	   printf("Pi is approximately %.16f, Error is %.16f\n", pi_estimate, fabs(pi_estimate - PI25DT));
-   }
-   MPI_Finalize(); 
-   return 0;
+    MPI_Reduce(&number_in_circle, &circles, 1, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        pi_estimate = (4*circles)/((double) number_of_tosses);
+        printf("Pi is approximately %.16f, Error is %.16f\n", pi_estimate, fabs(pi_estimate - PI25DT));
+    }
+    MPI_Finalize(); 
+    return 0;
 
 }
 
