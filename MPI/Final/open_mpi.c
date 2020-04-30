1dfd689d9388a30848050a7f053207dadc79d362
PumkinQ <nz9024295@gmail.com>
2020-04-29 21:51:57 -0500

    open_mpi.c

--- MPI/Final/open_mpi.c
+++ MPI/Final/open_mpi.c
@@ -9,6 +9,12 @@
 #include <time.h>
 #include <math.h>
 
+/*------------------------------------------------------------------
+ * Function:     toss_darts
+ * Purpose:      toss darts randomly to find out the number in circles.
+ * Input args:   total_tosses:  number of tosses
+ * Output args:  circles:  the total number of circles
+ */
 long long toss_darts (long long total_tosses, long long* circles){
 	long long i, number_in_circle = 0;        
 	double x, y;
@@ -36,7 +42,7 @@ int main(int argc, char* argv[]){
     double pi_estimate;
     double PI25DT = 3.141592653589793238462643;
 
-   // Get input from the command line
+   /* Get input from the command line */
    if (argc != 3){
         printf("usage: ./ open_mpi <thread_count> <number_of_tosses> \n");
         exit(-1);
@@ -47,7 +53,8 @@ int main(int argc, char* argv[]){
     # pragma omp parallel num_threads(thread_count)
     toss_darts(number_of_tosses, &circles);
 
+    /* Estimate PI using the formula*/
     pi_estimate = (4*circles)/((double) number_of_tosses);
 	printf("Pi is approximately %.16f, Error is %.16f\n", pi_estimate, fabs(pi_estimate - PI25DT));
     return 0;
-}
+}/*  main  */
